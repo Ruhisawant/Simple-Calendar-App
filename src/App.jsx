91c8f8e5bc42fb38561aaa3aef6e541a3bbc6e48
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-06-02 00:32:53 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Calendar from './components/Calendar'
+
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1></h1>
+      <h2><Calendar /></h2>
+    </div>
+  )
+}
+
+export default App
