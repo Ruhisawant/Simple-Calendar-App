1f4be4cb7fe49e713cd1b858eb245187e8d2df0b
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-06-02 00:54:12 -0400

    Finished calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Calendar from './components/Calendar'
-
-import './App.css';
+import './App.css'
 
 const App = () => {
 
   return (
-    <div className="App">
-      <h1></h1>
-      <h2><Calendar /></h2>
+    <div className='App'>
+      <h1>Itinerary for 7 Days in Chicago 🏙</h1>
+      <h2>Welcome to Chicago! You've got a week full of food and fun.</h2>
+      <Calendar />
     </div>
   )
 }
